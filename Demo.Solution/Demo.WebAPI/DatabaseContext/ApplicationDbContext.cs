b7f39d12fd90e0c523aaba1a070dc599cd51e8df
marcosbuydid <marcosbuydid@gmail.com>
2024-03-06 17:28:38 -0300

    configure db access and make initial migration

---
+++ Demo.Solution/Demo.WebAPI/DatabaseContext/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Demo.WebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Demo.WebAPI.DatabaseContext {
+    public class ApplicationDbContext : DbContext {
+        public ApplicationDbContext(DbContextOptions options) : base(options) { }
+
+        public ApplicationDbContext() {}
+
+        public virtual DbSet<City> Cities { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Demo.Solution/Demo.WebAPI/Models/City.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Demo.WebAPI.Models
+{
+    public class City
+    {
+        [Key]
+        public Guid CityID { get; set; }
+
+        public string? CityName { get; set; }
+    }
+}
