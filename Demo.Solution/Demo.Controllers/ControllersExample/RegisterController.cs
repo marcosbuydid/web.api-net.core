1f2360d3970c7df9ac2c2e3954e49560291bb19b
marcosbuydid <marcosbuydid@gmail.com>
2024-03-08 21:12:59 -0300

    start working with model validation

---
+++ Demo.Solution/Demo.Controllers/ControllersExample/RegisterController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Demo.Controllers.Models;
+
+namespace Demo.Controllers.ControllersExample
+{
+    public class RegisterController : Controller
+    {
+
+        [Route("/api/register")]
+
+        public IActionResult Index(Person person)
+        {
+            if (!ModelState.IsValid)
+            {
+                string errors = string.Join("\n",
+                    ModelState.Values.SelectMany(
+                        value => value.Errors).Select(
+                        e => e.ErrorMessage));
+                return BadRequest(errors);
+            }
+
+            return new JsonResult(person);
+        }
+    }
+}
--- Demo.Solution/Demo.Controllers/Models/Person.cs
+++ Demo.Solution/Demo.Controllers/Models/Person.cs
@@ -1,9 +1,13 @@
-﻿namespace Demo.Controllers.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Demo.Controllers.Models
 {
     public class Person
     {
-        public Guid Id { get; set; }
+        public Guid Id { get; set; } = Guid.NewGuid();
+        [Required(ErrorMessage = "First Name cannot be empty or null")]
         public string? FirstName { get; set; }
+        [Required(ErrorMessage = "Last Name cannot be empty or null")]
         public string? LastName { get; set; }
         public int Age { get; set; }
     }
