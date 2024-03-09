1fafb71a152089efa06824fd62ed747952be1da5
marcosbuydid <marcosbuydid@gmail.com>
2024-03-09 20:22:08 -0300

    create new web api proyect

---
+++ Demo.Solution/Demo.Web.API/DatabaseContext/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Demo.Web.API.Models;
+using Microsoft.EntityFrameworkCore;
+namespace Demo.Web.API.DatabaseContext
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options) { }
+
+        public ApplicationDbContext() { }
+
+        public virtual DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
---
+++ Demo.Solution/Demo.Web.API/Models/UserDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Demo.Web.API.Models
+{
+    public record UserDTO
+    {
+        [Required(ErrorMessage = "Email cannot be empty or null")]
+        [EmailAddress(ErrorMessage = "Verify your email format")]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "Password cannot be empty or null")]
+        [StringLength(40, MinimumLength = 8, ErrorMessage = "Password should have 8 to 40 chars")]
+        public string? Password { get; set; }
+    }
+}
