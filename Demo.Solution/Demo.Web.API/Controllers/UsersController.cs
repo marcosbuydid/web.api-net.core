4f5c1fbb6b0b446dbad2d4a9de424cc44b729b8e
marcosbuydid <marcosbuydid@gmail.com>
2024-03-11 22:55:47 -0300

    add authorization to create user endpoint

--- Demo.Solution/Demo.Web.API/Controllers/UsersController.cs
+++ Demo.Solution/Demo.Web.API/Controllers/UsersController.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Demo.Web.API.DatabaseContext;
 using Demo.Web.API.Models;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
 
 namespace Demo.Web.API.Controllers
 {
@@ -74,12 +76,39 @@ namespace Demo.Web.API.Controllers
 
         // POST: api/Users
         [HttpPost]
-        public async Task<ActionResult<User>> PostUser(User user)
+        [Authorize]
+        public async Task<ActionResult<User>> PostUser([FromBody] User userToCreate)
         {
-            _context.Users.Add(user);
-            await _context.SaveChangesAsync();
+            //[Authorize(Roles = "Administrator")]
+            //if the user is not administrator return automatically
+            //forbidden result and not enters to the endpoint.
+            //is a better solution and for custom messages middlewares are used.
 
-            return CreatedAtAction("GetUser", new { id = user.Id }, user);
+            //Authorize
+            var identity = HttpContext.User.Identity as ClaimsIdentity;
+
+            var userClaims = identity.Claims;
+            var role = userClaims.FirstOrDefault(x => x.Type == ClaimTypes.Role)?.Value;
+
+            if (role == "Administrator")
+            {
+                var user = _context.Users.Where(user => user.Email == userToCreate.Email).FirstOrDefault();
+                if (user != null)
+                {
+                    return StatusCode(201, "Cannot create user, email is already registered");
+                }
+
+                _context.Users.Add(userToCreate);
+                await _context.SaveChangesAsync();
+
+                return CreatedAtAction("GetUser", new { id = userToCreate.Id }, userToCreate);
+
+            }
+            else
+            {
+                return StatusCode(403, "Only Administrators can create users");
+            }
+
         }
 
         // DELETE: api/Users/5
